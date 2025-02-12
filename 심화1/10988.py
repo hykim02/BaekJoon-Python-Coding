5898a269d3b438329bce68e55caa420ccbdd58bf
hykim02 <hykim4435@naver.com>
2025-02-12 23:21:39 +0900

    [feat]success

--- 심화1/10988.py
+++ 심화1/10988.py
@@ -1,14 +1,7 @@
 # 팰린드롬인지 확인하기
-word = input()
-word_list = list(word)
+word = list(str(input()))
 
-# 역전시키기
-word_list.reverse()
-
-print(word)
-print(word_list)
-# 비교
-if(word == word_list):
+if list(reversed(word)) == word:
     print(1)
 else:
     print(0)
